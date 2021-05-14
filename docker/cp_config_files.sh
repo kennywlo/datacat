274784ba8d687718e65963fbcd7e9afc81e6ec4b
Kenny Lo <kennylo@slac.stanford.edu>
2021-05-14 14:33:34 -0700

    Fix dont overwrite prompt

--- docker/cp_config_files.sh
+++ docker/cp_config_files.sh
@@ -2,10 +2,10 @@
 # Author: Kenny Lo <kennylo@slac.stanford.edu>, 2021
 
 # copy out the files needed in Tomcat
-cp -f /opt/datacat/docker/tomcat/server.xml /tmp/server.xml
-cp -f /opt/datacat/docker/tomcat/java.security /tmp/java.security
-cp -f /opt/datacat/webapp/target/org-srs-webapps-datacat-0.6-DEPENDENCY.war /tmp/datacat.war
-cp -f ~/.m2/repository/mysql/mysql-connector-java/8.0.16/mysql-connector-java-8.0.16.jar /tmp/mysql-connector-java-8.0.16.jar
+/bin/cp -f /opt/datacat/docker/tomcat/server.xml /tmp/server.xml
+/bin/cp -f /opt/datacat/docker/tomcat/java.security /tmp/java.security
+/bin/cp -f /opt/datacat/webapp/target/org-srs-webapps-datacat-0.6-DEPENDENCY.war /tmp/datacat.war
+/bin/cp -f ~/.m2/repository/mysql/mysql-connector-java/8.0.16/mysql-connector-java-8.0.16.jar /tmp/mysql-connector-java-8.0.16.jar
 
 # to keep container alive for testing purpose
 tail -F /dev/null
