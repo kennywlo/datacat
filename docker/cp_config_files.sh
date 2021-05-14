72248498f837bd44dc29816f931b62e8aefad003
Kenny Lo <kennylo@slac.stanford.edu>
2021-05-14 14:48:22 -0700

    Use matching names for for copying files out of container

--- docker/cp_config_files.sh
+++ docker/cp_config_files.sh
@@ -4,7 +4,8 @@
 # copy out the files needed in Tomcat
 /bin/cp -f /opt/datacat/docker/tomcat/server.xml /tmp/server.xml
 /bin/cp -f /opt/datacat/docker/tomcat/java.security /tmp/java.security
-/bin/cp -f /opt/datacat/webapp/target/org-srs-webapps-datacat-0.6-DEPENDENCY.war /tmp/datacat.war
+/bin/mv /opt/datacat/webapp/target/org-srs-webapps-datacat-0.6-DEPENDENCY.war /opt/datacat/webapp/target/datacat.war
+/bin/cp -f /opt/datacat/webapp/target/datacat.war /tmp/datacat.war
 /bin/cp -f ~/.m2/repository/mysql/mysql-connector-java/8.0.16/mysql-connector-java-8.0.16.jar /tmp/mysql-connector-java-8.0.16.jar
 
 # to keep container alive for testing purpose
